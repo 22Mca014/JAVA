8cc8431945c2f330f5c6519950b466a3917a45f6
DavidNitian <d12mondal@gmail.com>
2024-11-14 20:47:58 +0530

    learning about printing

---
+++ print.java
@@ -0,0 +1,13 @@
+public class print {
+//this a block.
+
+     public static void main(String[] args) {
+        int a=5;
+        String name ="David";
+        //printing method.
+        /*1. */   System.out.println("David");
+      /*2 */     System.out.printf("Name:%s ,Age:%d\n",name,a);
+      /*3 */System.out.println("name:"+name+" \n"+"Age:"+a);
+     }
+     //commonly we can use 3rd method
+}
