a4384435cbc286a30f5b8c60ab8fc254e3dc95b7
DavidNitian <d12mondal@gmail.com>
2024-11-14 21:05:06 +0530

    2

---
+++ datatype.java
@@ -0,0 +1,11 @@
+public class datatype {
+    public static void main(String[] args) {
+        int  a=5;
+        char  dop= 'D';
+        byte b=14;
+        float f=2.14f;
+        double du=12.99;
+
+
+}
+}
